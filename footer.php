fdc019a854b5625e3095452923606438006168b8
Mika Epstein (Ipstenu) <ipstenu@ipstenu.org>
2013-06-25 13:48:20 -0700

    Typo

--- footer.php
+++ footer.php
@@ -37,7 +37,7 @@
 </div>
 </div>
 
-<div id="footer" class="footer"><div class="wrap"><div class="menu-subsidiary-container"><ul id="menu-subsidiary" class="menu"><li id="item-id" class="menu-item menu-item-type-custom">Copyright © 1995-<?php echo date("Y"); ?> <em><a href="http://jorjafox.net/">Jorja Fox: Online</a></em></li><li id="menu-item-3597" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3597"><a href="http://jorjafox.net/wiki/JorjaPedia:General_disclaimer">Disclaimer</a></li>
+<div id="footer" class="footer"><div class="wrap"><div class="menu-subsidiary-container"><ul id="menu-subsidiary" class="menu"><li id="item-id" class="menu-item menu-item-type-custom">Copyright © 1996-<?php echo date("Y"); ?> <em><a href="http://jorjafox.net/">Jorja Fox: Online</a></em></li><li id="menu-item-3597" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3597"><a href="http://jorjafox.net/wiki/JorjaPedia:General_disclaimer">Disclaimer</a></li>
 <li id="menu-item-3598" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3598"><a href="http://jorjafox.net/wiki/JorjaPedia:Copyrights">Copyright</a></li>
 <li id="menu-item-3599" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3599"><a href="http://jorjafox.net/wiki/JorjaPedia:Policy">Policy</a></li>
 <li id="menu-item-3683" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3683"><a href="http://jorjafox.net/link-back/">Link Back</a></li>
