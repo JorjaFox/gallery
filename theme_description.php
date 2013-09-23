ddacb9279cec9c69b7c025b2e84fa214a204a883
Ipstenu <ipstenu@ipstenu.org>
2013-09-23 13:08:38 -0700

    Going Green

--- theme_description.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Zenphoto theme definition file
-$theme_description['name'] = 'JFO Genesis';
-$theme_description['author'] = 'Ipstenu';
-$theme_description['version'] = '2.2';
-$theme_description['date'] = '09/19/2013';
-$theme_description['desc'] = gettext("Example Theme modded heavily to be wrapped by Genesis Framework.");
-?>
