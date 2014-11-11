4d8cf4f9ace7f2fbf854c84cf9d4db605d17e27e
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2014-11-10 19:47:21 -0800

    Updating desc.

--- theme_description.php
+++ theme_description.php
@@ -2,7 +2,7 @@
 // Zenphoto theme definition file
 $theme_description['name'] = 'JFO Genesis';
 $theme_description['author'] = 'Ipstenu';
-$theme_description['version'] = '3.3.0';
-$theme_description['date'] = '10/29/2014';
+$theme_description['version'] = '3.3.1';
+$theme_description['date'] = '11/10/2014';
 $theme_description['desc'] = gettext("Example Theme modded heavily to be wrapped by Genesis Framework.");
 ?>
