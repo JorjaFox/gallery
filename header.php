47017f9d0ab6ff36fb1727914d758079044ce521
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-01-16 23:50:16 -0800

    Merging with Utility

--- header.php
+++ header.php
@@ -1,27 +1,34 @@
 	<?php include("/home/jorjafox/public_html/content/code/ads/projectwonderful.php"); ?>
 
-<div class="site-container">
-    <nav class="nav-primary" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement"><div class="wrap">
-        <ul id="menu-header" class="menu genesis-nav-menu menu-primary">
-            <li id="menu-item-6105" class="menu-home menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-6105"><a href="http://jorjafox.net">Home</a></li>
-            <li id="menu-item-3622" class="menu-photos menu-item menu-item-type-custom menu-item-object-custom menu-item-3622 current-menu-item current_page_item"><a href="http://jorjafox.net/gallery">Photos</a></li>
-            <li id="menu-item-3623" class="menu-videos menu-item menu-item-type-custom menu-item-object-custom menu-item-3623"><a href="http://jorjafox.net/video/">Videos</a></li>
-            <li id="menu-item-3624" class="menu-jorjapedia menu-item menu-item-type-custom menu-item-object-custom menu-item-3624"><a href="http://jorjafox.net/wiki/Main_Page">JorjaPedia</a></li>
-            <li id="menu-item-8517" class="top menu-item menu-item-type-custom menu-item-object-custom menu-item-8517"><a title="Jump to Top" href="#top">Top</a></li>
-        </ul>
-    </div></nav>
+	<?php include("/home/jorjafox/public_html/content/code/ads/projectwonderful.php"); ?>
 
-	<header class="site-header" role="banner" itemscope="itemscope" itemtype="http://schema.org/WPHeader"><div class="wrap">
-    	<div class="title-area"><p id="header-image"><!-- Logo --></p></div><!-- end #title-area -->
-    	<aside class="widget-area header-widget-area"><section id="text-4" class="widget widget_text"><div class="widget-wrap">
-    		<div class="textwidget">
-    		<div class="site-intro">
-    		    <h1><a href="http://jorjafox.net">Jorja Fox: Online</a></h1>
-                <p>A fansite for everything<br />(about Jorja Fox)</p>
-            </div>
-            <div class="pw-header"><?php include("/home/jorjafox/public_html/content/code/ads/donate.php"); ?></div>
-            </div>
-        </div></section></aside>
-	</div></header><!--end #header-->
+<ul class="wpacc-genesis-skip-link">
+  <li><a href="#genwpacc-genesis-nav-primary" class="screen-reader-shortcut">Skip to primary navigation</a></li>
+  <li><a href="#genwpacc-genesis-content" class="screen-reader-shortcut">Skip to content</a></li>
+  <li><a href="#genwpacc-sidebar-primary" class="screen-reader-shortcut">Skip to primary sidebar</a></li>
+  <li><a href="#genwpacc-genesis-footer-widgets" class="screen-reader-shortcut">Skip to footer widgets</a></li>
+  <li><a href="#genwpacc-genesis-nav-footer" class="screen-reader-shortcut">Skip to footer navigation</a></li>
+</ul>
 
-    <div class="site-inner"><div class="wrap"><div class="content-sidebar-wrap">
+<header class="site-header" role="banner" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
+	<div class="wrap">
+		<div class="title-area"><h1 class="site-title" itemprop="headline"><a href="http://jorjafox.net/">Jorja Fox: Online</a></h1>
+		<p class="site-description" itemprop="description">A fansite for LeFox, Jorja Fox</p></div>
+		<aside class="widget-area header-widget-area"><section id="text-17" class="widget widget_text"><div class="widget-wrap"><div class="textwidget"><img src="/content/code/rotate-logo/rotate.php"></div>
+		</div></section></aside>
+	</div>
+</header><!--end #header-->
+
+SiteNavigationElement" id="genwpacc-genesis-nav-primary">
+	<div class="wrap">
+		<ul id="menu-header" class="menu genesis-nav-menu menu-primary">
+			<li id="menu-item-6105" class="menu-home menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-6105"><a href="http://jorjafox.net">Home</a></li>
+			<li id="menu-item-3622" class="menu-photos menu-item menu-item-type-custom menu-item-object-custom menu-item-3622"><a href="http://jorjafox.net/gallery">Photos</a></li>
+			<li id="menu-item-3623" class="menu-videos menu-item menu-item-type-custom menu-item-object-custom menu-item-3623"><a href="http://jorjafox.net/video/">Videos</a></li>
+			<li id="menu-item-3624" class="menu-jorjapedia menu-item menu-item-type-custom menu-item-object-custom menu-item-3624"><a href="http://jorjafox.net/wiki/Main_Page">JorjaPedia</a></li>
+		</ul>
+	</div>
+</nav>
+
+<div class="site-inner utility-pro-green"><div class="wrap">
+	<div class="content-sidebar-wrap">
--- sidebar.php
+++ sidebar.php
@@ -1,87 +1,30 @@
-<aside class="sidebar sidebar-primary widget-area" role="complementary" itemscope="itemscope" itemtype="http://schema.org/WPSideBar">
+		<aside class="sidebar sidebar-primary widget-area" role="complementary" itemscope="itemscope" itemtype="http://schema.org/WPSideBar" id="genwpacc-sidebar-primary">
 
-    <section id="text-8" class="widget widget_text"><div class="widget-wrap">
-    	<div class="textwidget"><div class="lq-content"><?php include("/home/jorjafox/public_html/content/code/ads/liquidweb-325x38.php"); ?></div><br /><div class="pw-sidebar"><div id="halfbanner-234x60"><?php include("/home/jorjafox/public_html/content/code/ads/halfbanner-234x60.php"); ?></div></div></div>
-    </div></section>
-    
-    <section id="nav_menu-3" class="widget widget_nav_menu"><div class="widget-wrap">
-        <div class="menu-secondary-container"><ul id="menu-secondary" class="menu">
-            <li id="menu-item-4225" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4225"><a href="http://jorjafox.net/wiki/JorjaPedia:About">About Us</a></li>
-            <li id="menu-item-4227" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4227"><a href="http://jorjafox.net/contact/">Contact Us</a></li>
-            <li id="menu-item-6103" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6103"><a href="http://jorjafox.net/support/">Support Us</a></li>
-            <li id="menu-item-4226" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4226"><a href="http://jorjafox.net/wiki/FAQs">Frequently Asked Questions</a></li>
-            <li id="menu-item-7233" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7233"><a href="http://jorjafox.net/updates">Subscribe to Updates</a></li>
-        </ul></div>
-    </div></section>
+            <section id="text-8" class="widget widget_text"><div class="widget-wrap">
+                <div class="textwidget"><div class="lq-content"><?php include("/home/jorjafox/public_html/content/code/ads/liquidweb-325x38.php"); ?></div><br /><div class="pw-sidebar"><div id="halfbanner-234x60"><?php include("/home/jorjafox/public_html/content/code/ads/halfbanner-234x60.php"); ?></div></div></div>
+            </div></section>
 
-    <section id="search-3" class="widget widget_search">
-        <div class="widget-wrap"><?php printSearchForm();  ?></div>
-    </section>
-    
-    <section id="nav_menu-4" class="widget widget_nav_menu"><div class="widget-wrap">
-        <h4 class="widget-title widgettitle">Follow Jorja Fox</h4>
-        <div class="menu-officially-jorja-container"><ul id="menu-officially-jorja" class="menu">
-			<li id="menu-item-house" class="social-buttons menu-item"><a href="http://jorjafox.com/" title="Jorja's Website">
-			
-			<span class="fa-stack fa-lg">
-			  <i class="fa fa-square fa-stack-2x"></i>
-			  <i class="fa fa-home fa-stack-1x fa-inverse"></i>
-			</span>
-			</a></li>
-			
-			<li id="menu-item-facebook-square" class="social-buttons menu-item"><a href="https://www.facebook.com/JorjaFoxWorldwide" title="Jorja on Facebook"><i class="fa fa-facebook-square"></i></a></li>
-			
-			<li id="menu-item-twitter" class="social-buttons menu-item"><a href="http://twitter.com/jorjafoxofficia" title="Jorja on Twitter"><i class="fa fa-twitter-square"></i></a></li>
-		</ul></div>
-    </div></section>
+            <section id="nav_menu-3" class="widget widget_nav_menu"><div class="widget-wrap">
+                <div class="menu-secondary-container"><ul id="menu-secondary" class="menu">
+                    <li id="menu-item-4225" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4225"><a href="http://jorjafox.net/wiki/JorjaPedia:About">About Us</a></li>
+                    <li id="menu-item-4227" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4227"><a href="http://jorjafox.net/contact/">Contact Us</a></li>
+                    <li id="menu-item-6103" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6103"><a href="http://jorjafox.net/support/">Support Us</a></li>
+                    <li id="menu-item-4226" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4226"><a href="http://jorjafox.net/wiki/FAQs">Frequently Asked Questions</a></li>
+                    <li id="menu-item-7233" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7233"><a href="http://jorjafox.net/updates">Subscribe to Updates</a></li>
+                </ul></div>
+            </div></section>
 
-    <section id="nav_menu-5" class="widget widget_nav_menu"><div class="widget-wrap">
-        <h4 class="widget-title widgettitle">Follow JFO</h4>
-        <div class="menu-officially-jfo-container"><ul id="menu-officially-jfo" class="menu">
-			<li id="menu-item-facebook" class="social-buttons menu-item"><a href="https://www.facebook.com/JFO.Fans" title="Facebook"><i class="fa fa-facebook-square"></i></a></li>
-			<li id="menu-item-googleplus" class="social-buttons menu-item"><a href="https://google.com/+JorjafoxNet" title="Google+"><i class="fa fa-google-plus-square"></i></a></li>
-			<li id="menu-item-twiter" class="social-buttons menu-item"><a href="http://twitter.com/jorjafoxonline" title="Twitter"><i class="fa fa-twitter-square"></i></a></li>
-			<li id="menu-item-tumblr" class="social-buttons menu-item"><a href="http://tumblr.jorjafox.net/" title="Tumblr"><i class="fa fa-tumblr-square"></i></a></li>
-			<li id="menu-item-vim" class="social-buttons menu-item"><a href="http://vimeo.com/jorjafox" title="Vimeo"><i class="fa fa-vimeo-square"></i></a></li>
-		</ul></div>
-    </div></section>
+		    <section id="text-11" class="widget widget_text"><div class="widget-wrap"><h4 class="widget-title widgettitle">Follow Jorja</h4>
+<div class="textwidget"><a href="http://jorjafox.com/" title="Jorja's Website"><i class="fa fa-home fa-home fa-3x"></i></a> <a href="https://www.facebook.com/JorjaFoxWorldwide" title="Jorja on Facebook"><i class="fa fa-facebook-square fa-3x"></i></a> <a href="http://twitter.com/jorjafoxofficia" title="Jorja on Twitter"><i class="fa fa-twitter-square fa-3x"></i></a> </div>
+</div></section>
 
-    <section id="nav_menu-6" class="widget widget_nav_menu"><div class="widget-wrap">
-        <h4 class="widget-title widgettitle">Keep JFO Running</h4>
-        <div class="menu-donations-container">
-        <ul id="menu-donate" class="menu">
-			<li id="menu-item-paypal" class="donation-buttons menu-item"><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=S7K469QF8NQ56" title="Donate via Paypal">
-			
-			<span class="fa-stack fa-lg">
-			  <i class="fa fa-square fa-stack-2x"></i>
-			  <i class="fa fa-money fa-stack-1x fa-inverse"></i>
-			</span>
-			
-			</a></li>
-			<li id="menu-item-halfelf" class="donation-buttons menu-item"><a href="https://store.halfelf.org/donate/" title="Donate via Halfelf (my store)">
-			
-			<span class="fa-stack fa-lg">
-			  <i class="fa fa-square fa-stack-2x"></i>
-			  <i class="fa fa-female fa-stack-1x fa-inverse"></i>
-			</span>
-			
-			
-			</a></li>
-			<li id="menu-item-amazon" class="donation-buttons menu-item"><a href="http://www.amazon.com/registry/wishlist/1S5TEEA510W19" title="Buy something off my Amazon Gift List">
-			
-			<span class="fa-stack fa-lg">
-			  <i class="fa fa-square fa-stack-2x"></i>
-			  <i class="fa fa-gift fa-stack-1x fa-inverse"></i>
-			</span>
-			
-			</a> </li>
-		</ul>
-        </div>
-    </div></section>
+		    <section id="text-10" class="widget widget_text"><div class="widget-wrap"><h4 class="widget-title widgettitle">Follow JFO</h4>
+			<div class="textwidget"><a href="https://www.facebook.com/JFO.Fans" title="Facebook"><i class="fa fa-facebook-square fa-3x"></i></a> <a href="https://google.com/+JorjafoxNet" title="Google+"><i class="fa fa-google-plus-square fa-3x"></i></a> <a href="http://twitter.com/jorjafoxonline" title="Twitter"><i class="fa fa-twitter-square fa-3x"></i></a> <a href="http://tumblr.jorjafox.net/" title="Tumblr"><i class="fa fa-tumblr-square fa-3x"></i></a> <a href="http://vimeo.com/jorjafox" title="Vimeo"><i class="fa fa-vimeo-square fa-3x"></i></a></div>
+			</div></section>
 
-    <section id="text-5" class="widget widget_text"><div class="widget-wrap">
-        <div class="textwidget">
-            <div class="pw-sidebar"><?php include("/home/jorjafox/public_html/content/code/ads/skyscraper-160x600.php"); ?></div>
-        </div>
-    </div></section>
+            <section id="text-5" class="widget widget_text"><div class="widget-wrap">
+                <div class="textwidget">
+                    <div class="pw-sidebar"><?php include("/home/jorjafox/public_html/content/code/ads/skyscraper-160x600.php"); ?></div>
+                </div>
+            </div></section>
 </aside>
