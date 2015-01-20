6b08f07dc2c17ef583c4a83d18393c0b926984cb
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-01-19 16:54:44 -0800

    Backstretch

--- _scripts.php
+++ _scripts.php
@@ -8,15 +8,15 @@
 
 	<!-- Scripts -->
 
-	<script type="text/psajs" orig_index="13">//<![CDATA[
+	<script type="text/psajs" orig_index="12">//<![CDATA[
 		var utilityResponsiveL10n={"buttonText":"Menu","buttonLabel":"Primary Navigation Menu","subButtonText":"Menu","subButtonLabel":"Sub Navigation Menu"};
 	//]]></script>
 
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/responsive-menu.js?ver=1.0.0' type="text/psajs" orig_index="13"></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/genwpacc-dropdown.js?ver=4.1' type="text/psajs" orig_index="14"></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/genwpacc-skiplinks.js?ver=1.0.0' type="text/psajs" orig_index="15"></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js?ver=2.0.1' type="text/psajs" orig_index="16"></script>
+	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/responsive-menu.js' type="text/psajs" orig_index="13"></script>
+	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/genwpacc-dropdown.js' type="text/psajs" orig_index="14"></script>
+	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/genwpacc-skiplinks.js' type="text/psajs" orig_index="15"></script>
+	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js' type="text/psajs" orig_index="16"></script>
 	<script type="text/psajs" orig_index="17">//<![CDATA[
 	var utilityBackstretchL10n={"src":"http:\/\/jorjafox.net\/wordpress\/wp-content\/uploads\/2015\/01\/JorjaFox.com_.jpg"};
 	//]]></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/backstretch.args.js?ver=1.0.0' type="text/psajs" orig_index="18"></script>
+	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/backstretch.args.js' type="text/psajs" orig_index="18"></script>
