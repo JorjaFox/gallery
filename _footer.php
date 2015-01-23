a8a9822a7dafb3d0b7ffc7649af9f337c25e69cb
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-01-22 17:43:29 -0800

    Fixing footer links

--- _footer.php
+++ _footer.php
@@ -1,10 +1,10 @@
     </div> <!-- content-sidebar-wrap -->
     </div><!-- end wrap --></div> <!-- end .site-inner -->
 
-	<nav class="nav-footer" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" id="genwpacc-genesis-nav-footer"><div class="wrap"><ul id="menu-footer" class="menu genesis-nav-menu menu-footer"><li id="menu-item-3597" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3597"><a href="wiki/JorjaPedia:General_disclaimer">Disclaimer</a></li>
-	<li id="menu-item-3598" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3598"><a href="wiki/JorjaPedia:Copyrights">Copyright</a></li>
-	<li id="menu-item-3599" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3599"><a href="wiki/JorjaPedia:Policy">Policy</a></li>
-	<li id="menu-item-3683" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3683"><a href="link-back/">Link Back</a></li>
+	<nav class="nav-footer" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" id="genwpacc-genesis-nav-footer"><div class="wrap"><ul id="menu-footer" class="menu genesis-nav-menu menu-footer"><li id="menu-item-3597" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3597"><a href="http://jorjafox.net/wiki/JorjaPedia:General_disclaimer">Disclaimer</a></li>
+	<li id="menu-item-3598" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3598"><a href="http://jorjafox.net/wiki/JorjaPedia:Copyrights">Copyright</a></li>
+	<li id="menu-item-3599" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3599"><a href="http://jorjafox.net/wiki/JorjaPedia:Policy">Policy</a></li>
+	<li id="menu-item-3683" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3683"><a href="http://jorjafox.net/link-back/">Link Back</a></li>
 	</ul></div></nav>
 
 	<footer class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
