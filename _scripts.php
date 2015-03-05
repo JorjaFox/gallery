01bcf61708161a9df373c42e2a397ef6198e5f37
Ipstenu <ipstenu@halfelf.org>
2015-03-04 16:18:33 -0800

    Combining min files properly to fix backstretch

--- _scripts.php
+++ _scripts.php
@@ -2,19 +2,22 @@
 
 	<!-- Stylesheets -->
 	<link rel='stylesheet' href='<?php echo $_zp_themeroot ?>/zen.css' type='text/css' />
-	<link rel='stylesheet' href='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/style.css' type='text/css' />
-	<link rel='stylesheet' href='http://jorjafox.net/content/code/fonts/fonts.css' type='text/css' />
+	<link rel='stylesheet' href='/wordpress/wp-content/themes/jfogenesis/style.css' type='text/css' />
+	<link rel='stylesheet' href='/content/code/fonts/fonts.css' type='text/css' />
 	<link type='text/plain' rel='author' href='http://jorjafox.net/humans.txt' />
 
 	<!-- Scripts -->
-
-	<script src='http://jorjafox.net/content/code/js/jfo-content.min.js' type="text/psajs" orig_index="13"></script>
-	<script type="text/psajs">//<![CDATA[
-		var utilityResponsiveL10n={"buttonText":"Menu","buttonLabel":"Primary Navigation Menu","subButtonText":"Menu","subButtonLabel":"Sub Navigation Menu"};
-	//]]></script>
-	<script type="text/psajs">//<![CDATA[
-		var utilityBackstretchL10n={"src":"http:\/\/jorjafox.net\/wordpress\/wp-content\/uploads\/2015\/01\/JorjaFox.com_.jpg"};
-	//]]></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/responsive-menu.min.js' type="text/psajs" orig_index="16"></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/genwpacc.min.js' type="text/psajs" orig_index="17"></script>
-	<script src='http://jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js' type="text/psajs" orig_index="19"></script>
+	<script type='text/javascript'>
+	/* <![CDATA[ */
+		var utilityResponsiveL10n = {"buttonText":"Menu","buttonLabel":"Primary Navigation Menu","subButtonText":"Menu","subButtonLabel":"Sub Navigation Menu"};
+	/* ]]> */
+	</script>
+	<script type='text/javascript' src='/wordpress/wp-content/themes/jfogenesis/js/responsive-menu.min.js'></script>
+	<script type='text/javascript' src='/wordpress/wp-content/themes/jfogenesis/js/genwpacc.min.js'></script>
+	<script type='text/javascript'>
+	/* <![CDATA[ */
+	var utilityBackstretchL10n = {"src":"http:\/\/jorjafox.net\/wordpress\/wp-content\/uploads\/2015\/01\/JorjaFox.com_.jpg"};
+	/* ]]> */
+	</script>
+	<script type='text/javascript' src='/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js'></script>
+	<script type='text/javascript' src='/content/code/js/jfo-content.min.js'></script>
