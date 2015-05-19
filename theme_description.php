613b4878bda95b479b3b7fdafff8d81f7057b965
Ipstenu <ipstenu@halfelf.org>
2015-05-19 12:52:51 -0700

    Version bump, add in code for referals

--- theme_description.php
+++ theme_description.php
@@ -2,7 +2,7 @@
 // Zenphoto theme definition file
 $theme_description['name'] = 'JFO Utility Pro';
 $theme_description['author'] = 'Ipstenu';
-$theme_description['version'] = '1.1.1';
-$theme_description['date'] = '03/30/2015';
+$theme_description['version'] = '1.0.2';
+$theme_description['date'] = '05/19/2015';
 $theme_description['desc'] = gettext("Zenphoto20 Fork of Carrie Dils' Utility Pro Theme.");
 ?>
