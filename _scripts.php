4c04771c02d2257e0e0dfa26edbf5014b59684d2
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-08-11 22:20:13 -0700

    Lets get this shit relative URLs so when I go https it’s all golden

--- _scripts.php
+++ _scripts.php
@@ -2,18 +2,18 @@
 
 	<!-- Stylesheets -->
 	<link rel='stylesheet' href='<?php echo $_zp_themeroot ?>/zen.css' type='text/css' />
-	<link rel='stylesheet' href='/wordpress/wp-content/themes/jfogenesis/style.css' type='text/css' />
+	<link rel='stylesheet' href='//jorjafox.net/wordpress/wp-content/themes/jfogenesis/style.css' type='text/css' />
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Enriqueta:400,700|Open+Sans:400italic,700italic,400,700">
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link type='text/plain' rel='author' href='http://jorjafox.net/humans.txt' />
 
 	<!-- Scripts -->
-	<script type='text/javascript' src='/wordpress/wp-content/themes/jfogenesis/js/genwpacc.min.js'></script>
+	<script type='text/javascript' src='//jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/genwpacc.min.js'></script>
 	<script type='text/javascript'>
 	/* <![CDATA[ */
 	//var utilityBackstretchL10n = {"src":"\/wordpress\/wp-content\/uploads\/2015\/01\/JorjaFox.com_.jpg"};
 	var utilityBackstretchL10n = {"src":"\/\/static.jorjafox.net\/content\/images\/background.jpg"};
 	/* ]]> */
 	</script>
-	<script type='text/javascript' src='/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js'></script>
-	<script type='text/javascript' src='/static/content/code/js/jfo-content.min.js'></script>
+	<script type='text/javascript' src='//jorjafox.net/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js'></script>
+	<script type='text/javascript' src='//static.jorjafox.net/content/code/js/jfo-content.min.js'></script>
