58f4563c23dfccd570dcab890224b22a2510cac7
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-08-29 10:58:06 -0700

    Widget moves

--- theme_description.php
+++ theme_description.php
@@ -2,7 +2,7 @@
 // Zenphoto theme definition file
 $theme_description['name'] = 'JFO Utility Pro';
 $theme_description['author'] = 'Ipstenu';
-$theme_description['version'] = '1.0.6';
-$theme_description['date'] = '08/11/2015';
+$theme_description['version'] = '1.0.3f';
+$theme_description['date'] = '08/29/2015';
 $theme_description['desc'] = gettext("Zenphoto20 Fork of Carrie Dils' Utility Pro Theme.");
 ?>
