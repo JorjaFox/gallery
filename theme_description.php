8602535fe23f582e2f34252fde216a1594d47096
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2016-04-26 13:44:50 -0700

    Version bump

--- theme_description.php
+++ theme_description.php
@@ -1,8 +1,8 @@
 <?php
 // Zenphoto theme definition file
-$theme_description['name'] = 'Utility Pro JF';
+$theme_description['name'] = 'Utility Pro - Jorja Fox';
 $theme_description['author'] = 'Ipstenu';
-$theme_description['version'] = '1.0.3i';
-$theme_description['date'] = '01/16/2016';
+$theme_description['version'] = '1.0.3j';
+$theme_description['date'] = '04/26/2016';
 $theme_description['desc'] = gettext("Zenphoto20 Fork of Carrie Dils' Utility Pro Theme.");
 ?>
