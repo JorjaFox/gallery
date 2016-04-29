73144792287bfda171f36effea3212aa2043b5c0
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2016-04-29 15:45:25 -0700

    Remove Font Awesome

--- _scripts.php
+++ _scripts.php
@@ -27,7 +27,6 @@
 	<link rel='stylesheet' href='<?php echo $_zp_themeroot ?>/zen.css' type='text/css' />
 	<link rel='stylesheet' href='/wordpress/wp-content/themes/jfogenesis/style.css' type='text/css' />
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Enriqueta:400,700">
-	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 	<link type='text/plain' rel='author' href='http://jorjafox.net/humans.txt' />
 
 	<!-- Scripts -->
