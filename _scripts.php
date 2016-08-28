f6b50701a186dadf9abe34bd721a8cef5ed30823
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2016-08-28 12:11:08 -0700

    static

--- _scripts.php
+++ _scripts.php
@@ -1,21 +1,21 @@
 <!-- Icons -->
-	<link rel="apple-touch-icon" sizes="57x57" href="https://jfo-static.net/images/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="https://jfo-static.net/images/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="https://jfo-static.net/images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="https://jfo-static.net/images/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="https://jfo-static.net/images/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="https://jfo-static.net/images/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="https://jfo-static.net/images/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="https://jfo-static.net/images/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="https://jfo-static.net/images/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="https://jfo-static.net/images/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="https://jfo-static.net/images/favicon-194x194.png" sizes="194x194">
-	<link rel="icon" type="image/png" href="https://jfo-static.net/images/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="https://jfo-static.net/images/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="https://jfo-static.net/images/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="https://jfo-static.net/images/manifest.json">
-	<link rel="mask-icon" href="https://jfo-static.net/images/safari-pinned-tab.svg" color="#5bbad5">
-	<link rel="shortcut icon" href="https://jfo-static.net/images/favicon.ico">
+	<link rel="apple-touch-icon" sizes="57x57" href="https://static.jorjafox.net/images/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="https://static.jorjafox.net/images/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="https://static.jorjafox.net/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="https://static.jorjafox.net/images/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="https://static.jorjafox.net/images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="https://static.jorjafox.net/images/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="https://static.jorjafox.net/images/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="https://static.jorjafox.net/images/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="https://static.jorjafox.net/images/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-194x194.png" sizes="194x194">
+	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="https://static.jorjafox.net/images/manifest.json">
+	<link rel="mask-icon" href="https://static.jorjafox.net/images/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="https://static.jorjafox.net/images/favicon.ico">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="msapplication-TileImage" content="/static/icons/mstile-144x144.png">
 	<meta name="msapplication-config" content="/static/icons/browserconfig.xml">
@@ -36,7 +36,7 @@
 	/* ]]> */
 	</script>
 	<script type='text/javascript' src='/wordpress/wp-content/themes/jfogenesis/js/backstretch.min.js'></script>
-	<script type='text/javascript' src='https://jfo-static.net/content/code/js/content.min.js'></script>
+	<script type='text/javascript' src='https://static.jorjafox.net/content/code/js/content.min.js'></script>
 	<?php
 		include($_SERVER["DOCUMENT_ROOT"]."/static/content/code/analyticstracking.php");
 	?>
