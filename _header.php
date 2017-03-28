eb0e49d6ae326cb50155fb6cc6f7145189c70593
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-03-27 20:37:11 -0700

    Using the real deal

--- _header.php
+++ _header.php
@@ -20,18 +20,8 @@
 		<section id="text-16" class="widget widget_text">
 			<div class="widget-wrap">
 				<div class="textwidget"><?php
-
-					$ch = curl_init();
-					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_URL, 'https://jorjafox.net/wp-json/wp/v2/pages/14363');
-					$result = curl_exec($ch);
-					curl_close($ch);
-
-					$obj = json_decode($result);
-
-					echo $obj->content->rendered;
-
+					$_GET['url'] = 'https://jorjafox.net/wp-json/wp/v2/pages/14363';
+					include($_SERVER["DOCUMENT_ROOT"]."/static/content/code/json/utility-bar.php");
 				?></div>
 			</div>
 		</section>
