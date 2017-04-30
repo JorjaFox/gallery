da3340f0858699f8cc6aa0698d7e4e00f93f870f
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-04-30 15:52:24 -0700

    Fixing enqueues

--- _scripts.php
+++ _scripts.php
@@ -1,24 +1,24 @@
 <!-- Icons -->
-	<link rel="apple-touch-icon" sizes="57x57" href="https://static.jorjafox.net/images/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="https://static.jorjafox.net/images/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="https://static.jorjafox.net/images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="https://static.jorjafox.net/images/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="https://static.jorjafox.net/images/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="https://static.jorjafox.net/images/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="https://static.jorjafox.net/images/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="https://static.jorjafox.net/images/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="https://static.jorjafox.net/images/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-194x194.png" sizes="194x194">
-	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="https://static.jorjafox.net/images/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="https://static.jorjafox.net/images/manifest.json">
-	<link rel="mask-icon" href="https://static.jorjafox.net/images/safari-pinned-tab.svg" color="#5bbad5">
-	<link rel="shortcut icon" href="https://static.jorjafox.net/images/favicon.ico">
+	<link rel="apple-touch-icon" sizes="57x57" href="/static/content/favicons/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/static/content/favicons/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/static/content/favicons/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/static/content/favicons/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/static/content/favicons/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/static/content/favicons/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/static/content/favicons/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/static/content/favicons/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/static/content/favicons/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="/static/content/favicons/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/static/content/favicons/favicon-194x194.png" sizes="194x194">
+	<link rel="icon" type="image/png" href="/static/content/favicons/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="/static/content/favicons/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="/static/content/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="/static/content/favicons/manifest.json">
+	<link rel="mask-icon" href="/static/content/favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="/static/content/favicons/favicon.ico">
 	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="msapplication-TileImage" content="/static/icons/mstile-144x144.png">
-	<meta name="msapplication-config" content="/static/icons/browserconfig.xml">
+	<meta name="msapplication-TileImage" content="/static/content/favicons/mstile-144x144.png">
+	<meta name="msapplication-config" content="/static/content/favicons/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
 
 	<meta name='viewport' content='width=device-width, initial-scale=1'/>
@@ -30,17 +30,24 @@
 	<link type='text/plain' rel='author' href='https://jorjafox.net/humans.txt' />
 
 	<!-- Scripts -->
+	<script type='text/javascript' src='/wordpress/wp-content/themes/utility-pro/js/backstretch.min.js'></script>
+	<script type='text/javascript'>
+		/* <![CDATA[ */
+		var utilityBackstretchL10n = {"src":"https:\/\/static.jorjafox.net\/content\/images\/JorjaFox.com_.jpg"};
+		/* ]]> */
+	</script>
+	<script type='text/javascript' src='/wordpress/wp-content/mu-plugins/utility-pro/backstretch.args.js'></script>
 	<script type='text/javascript' src='/wordpress/wp-content/themes/utility-pro/js/genwpacc-dropdown.js'></script>
+	<script type='text/javascript'>
+		/* <![CDATA[ */
+		var utilityMenuPrimaryL10n = {"buttonText":"Menu","buttonLabel":"Primary Navigation Menu","subButtonText":"Sub Menu","subButtonLabel":"Sub Menu"};
+		var utilityMenuFooterL10n = {"buttonText":"Footer Menu","buttonLabel":"Footer Navigation Menu","subButtonText":"Sub Menu","subButtonLabel":"Sub Menu"};
+		/* ]]> */
+	</script>
 	<script type='text/javascript' src='/wordpress/wp-content/themes/utility-pro/js/responsive-menu.min.js'></script>
 	<script type='text/javascript' src='/wordpress/wp-content/themes/utility-pro/js/responsive-menu.args.js'></script>
-	<script type='text/javascript'>
-	/* <![CDATA[ */
-	var utilityBackstretchL10n = {"src":"https:\/\/static.jorjafox.net\/content\/images\/JorjaFox.com_.jpg"};
-	/* ]]> */
-	</script>
-	<script type='text/javascript' src='/wordpress/wp-content/themes/utility-pro/js/backstretch.min.js'></script>
-	<script type='text/javascript' src='/wordpress/wp-content/mu-plugins/utility-pro/backstretch.args.js'></script>
-	<script type='text/javascript' src='https://static.jorjafox.net/content/code/js/content.min.js'></script>
+
+	<script type='text/javascript' src='/static/content/code/js/slide-up.js'></script>
 	<?php
 		include($_SERVER["DOCUMENT_ROOT"]."/static/content/code/analyticstracking.php");
 	?>
