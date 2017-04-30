bbbf33d6abca3f0769f046f14695a9fa46d7541a
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-04-30 14:18:37 -0700

    Menu changes - fixing links

--- _footer.php
+++ _footer.php
@@ -62,7 +62,7 @@
 	</div></nav>
 
 	<footer class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
-		<div class="wrap"><p>Copyright &copy; 1996-<?php echo date("Y"); ?> <em><a href="/">Fans of LeFox</a></em><br />Powered by <a href="https://http://zenphoto20.us/">ZenPhoto20</a> & <a href="http://www.shareasale.com/r.cfm?b=778546&u=728549&m=61628&urllink=&afftrack=">Utility Pro</a>& <a href="http://helf.us/genesis/">Genesis Framework</a>.<br>Hosted by <a href="https://liquidweb.evyy.net/c/294289/297312/4464">Liquidweb</a>.</p></div>
+		<div class="wrap"><p>Copyright &copy; 1996-<?php echo date("Y"); ?> <em><a href="/">Fans of LeFox</a></em><br />Powered by <a href="https://http://zenphoto20.us/">ZenPhoto20</a> & <a href="http://www.shareasale.com/r.cfm?b=778546&u=728549&m=61628&urllink=&afftrack=">Utility Pro</a> & <a href="http://helf.us/genesis/">Genesis Framework</a>.<br>Hosted by <a href="https://liquidweb.evyy.net/c/294289/297312/4464">Liquidweb</a>.</p></div>
 		<div class="adboxes-footer">
 			<?php
 				$_GET['name'] = 'leaderboard-728x90';
--- _sidebar.php
+++ _sidebar.php
@@ -2,13 +2,13 @@
             <section id="nav_menu-3" class="widget widget_nav_menu"><div class="widget-wrap">
 	            <h3 class="widgettitle widget-title">Information</h3>
                 <div class="menu-secondary-container"><ul id="menu-secondary" class="menu">
-	                <li id="menu-item-13467" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13467"><a href="https://jorjafox.net/about">About This Site</a></li>
+	                <li id="menu-item-13467" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13467"><a href="/about/">About This Site</a></li>
                     <li id="menu-item-4227" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4227"><a href="/contact/">Contact Us</a></li>
                     <li id="menu-item-4226" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4226"><a href="/library/faq/">Frequently Asked Questions</a></li>
                 </ul></div>
             </div></section>
 
-            <section id="text-5" class="widget widget_text"><div class="widget-wrap">
+            <section id="text-15" class="widget widget_text"><div class="widget-wrap">
                 <div class="textwidget">
                     <div class="adboxes-sidebar">
 	                    <?php
