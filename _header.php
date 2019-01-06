d026754c46cae66d84db7f2c7b8ceb37dbbd696f
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2019-01-05 19:05:10 -0800

    Header

--- _header.php
+++ _header.php
@@ -5,6 +5,8 @@
 	zp_apply_filter('theme_body_open');
 ?>
 
+<div class="authority-top-banner" style="display: block;"><div id="logo"><img src="https://static.jorjafox.net/wordpress/2019/01/cropped-flf-logo.png"></div></div>
+
 <div class="site-container">
 
 	<h2 id="additional-menu-label" class="screen-reader-text">Additional menu</h2>
