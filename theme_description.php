c563faa273d45a5d4a24f4bae7575f5ecf32d33e
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2021-08-28 20:06:30 -0700

    Image max height

--- theme_description.php
+++ theme_description.php
@@ -3,6 +3,6 @@
 // Zenphoto theme definition file
 $theme_description['name']    = 'Fans of LeFox';
 $theme_description['author']  = 'Mika E. (Ipstenu)';
-$theme_description['version'] = '2.0.6';
+$theme_description['version'] = '2.0.7';
 $theme_description['date']    = '2021-08-14';
 $theme_description['desc']    = 'Fork of the current Fans of LeFox theme for use on NetPhotoGraphics.';
