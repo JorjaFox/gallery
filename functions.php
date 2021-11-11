756439b0507656875807acfbf546f882068620bf
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2021-11-11 13:32:43 -0800

    Remove mobile code we're not using

--- functions.php
+++ functions.php
@@ -4,18 +4,3 @@ error_reporting( 0 );
 define( 'FLF_STABLE_THEME', '2.0.4' );
 // This should resolve to /home/me/public_html/
 define( 'FLF_BASE_DIR', dirname( SERVERPATH ) );
-
-// Check for mobile and tablets, and set some options
-require_once CORE_SERVERPATH . PLUGIN_FOLDER . '/mobileTheme/Mobile_Detect.php';
-$detect = new Mobile_Detect();
-
-if ( $detect->isTablet() ) {
-	$isTablet = true;
-} else {
-	$isTablet = false;
-}
-if ( ( $detect->isMobile() ) && ( ! $detect->isTablet() ) ) {
-	$isMobile = true;
-} else {
-	$isMobile = false;
-}
